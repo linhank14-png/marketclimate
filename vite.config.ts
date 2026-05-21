2ecf0ccaa308e982e88da38b6661896dbc85a733
linhank14-png <linhank14@gmail.com>
2026-05-21 11:50:31 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base:'/marketclimate/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
